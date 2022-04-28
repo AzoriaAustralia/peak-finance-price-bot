168d9c0ff1650a44bba8cb1e5167ec904a794425
Joshua Wallace <79308061+exp0logy@users.noreply.github.com>
2022-04-28 20:57:09 +0800

    ProFee Bug (Still to fix)

--- commands/tax.js
+++ commands/tax.js
@@ -9,11 +9,14 @@ const provider = new ethers.providers.WebSocketProvider('wss://andromeda-ws.meti
 const peak = new ethers.Contract(Peak.address, Peak.abi, provider);
 const pro = new ethers.Contract(Pro.address, Pro.abi, provider);
 
-async function proFee(){
-    let fee = await pro.maxFeeAmount()
-    let proFee = Number.parseInt(ethers.utils.formatEther(fee));
-    return proFee > 0 ? "ON" : "OFF"
-};
+// async function proFee(){
+//     let fee1 = await pro.treasuryRate().then(tax => ethers.BigNumber.toString(tax));
+//     let fee2 = await pro.buybackRate().then(tax => ethers.utils.formatUnits(tax, 'gwei'));
+//     console.log(fee1);
+//     console.log(fee2);
+//     let proFee = Number.parseInt(ethers.utils.formatEther(fee));
+//     return proFee > 0 ? "ON" : "OFF"
+// };
 
 async function peakFee() {
     let tax = await peak.taxRate().then(tax => ethers.utils.formatUnits(tax, 'wei'));
@@ -26,13 +29,14 @@ module.exports = {
         .setName('tax')
         .setDescription('Shows tax status and details'),
     async execute(interaction) {
+        // proFee()
         let peak = await peakFee();
-        let pro = await proFee()
+        // let pro = await proFee()
         let taxEmbed = new MessageEmbed()
             .setColor('#04E09F')
             .setFooter({ text: 'Peak Finance', iconURL: 'https://peakfinance.io/wp-content/uploads/2022/03/Logo-medium-.png' })
             .addField('$Peak Tax',  '```' + peak + '%```', false)
-            .addField('$Pro tax','```' + pro + '```', false);
+            .addField('$Pro tax','```' + 'Exp0logy broke this!' + '```', false);
         return interaction.reply({ embeds: [taxEmbed] })
     }
 }
